d9d37aed3232eff600445509defc40f4da642864
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-12-05 10:38:33 +0800

    JSAAL-3 (#3)
    * file structure: renamed App.js to App.jsx to follow eslint rules
    
    * JSAAL-3: finished setting up navigation
    
    * JSAAL-9 initialized redux
    
    * Update src/account/index.jsx
    
    Co-Authored-By: István Kárász <5849426+ikarasz@users.noreply.github.com>

---
+++ src/watchlists/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import commonStyles from '../common/styles';
+
+export default () => (
+  <View style={commonStyles.container}>
+    <Text>This is the watchlists screen</Text>
+  </View>
+);
