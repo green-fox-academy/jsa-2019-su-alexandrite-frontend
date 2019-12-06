ed32699e7eb0855ac25fa7648c2604a473aad9e7
Qiongying Wu <qywumail@gmail.com>
2019-12-06 18:20:33 +0800

    JSAAL-4 search page styling

--- src/search/result.jsx
+++ src/search/result.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+import tradeIcon from '../../assets/icons/search-page/trade.png';
+import detailIcon from '../../assets/icons/search-page/detail.png';
 
 export default () => {
   const isLoading = useSelector((state) => state.search.isLoading);
@@ -16,9 +19,26 @@ export default () => {
   return (
     <View>
       {searchResult.map((info) => (
-        <View>
-          <Text>{info.symbol}</Text>
-          <Text>{info.exchange}</Text>
+        <View
+          key={info.symbol}
+          style={{
+            paddingHorizontal: 17,
+            paddingVertical: 11,
+            borderBottomColor: '#eeeeee',
+            borderBottomWidth: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 12 }}>{info.symbol}</Text>
+            <Text style={{ fontSize: 10, color: '#999999' }}>{info.exchange}</Text>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <Image source={tradeIcon} />
+            <Image style={{ marginLeft: 26 }} source={detailIcon} />
+          </View>
         </View>
       ))}
     </View>
