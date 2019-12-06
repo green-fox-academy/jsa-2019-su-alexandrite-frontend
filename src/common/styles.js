aeb98b0e88189ca723b58f643cc43ee2cf514879
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-12-06 16:24:37 +0800

    JSAAL-27 created a generic card component

--- src/common/styles.js
+++ src/common/styles.js
@@ -7,4 +7,24 @@ export default StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: {
+      x: 0,
+      y: 4,
+    },
+    shadowRadius: 6,
+    padding: 15,
+    margin: 15,
+    backgroundColor: '#fff',
+    borderRadius: 6,
+  },
+  cardTitle: {
+    fontSize: 13,
+    lineHeight: 18,
+    fontWeight: '600',
+    color: '#333',
+    marginBottom: 15,
+  },
 });
