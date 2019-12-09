2caea07de634522cc51446ce6d32a10852ef71dc
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-12-09 11:51:14 +0800

    using flatlist for search results

--- src/search/result.jsx
+++ src/search/result.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { View, Text, Image, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import styles from './styles';
 
 import tradeIcon from '../../assets/icons/search-page/trade.png';
 import detailIcon from '../../assets/icons/search-page/detail.png';
+
+const ResultItem = ({ symbol, exchange }) => (
+  <View style={styles.resultList}>
+    <View>
+      <Text style={styles.stockName}>{symbol}</Text>
+      <Text style={styles.stockExchange}>{exchange}</Text>
+    </View>
+    <View style={styles.resultButton}>
+      <TouchableHighlight>
+        <Image source={tradeIcon} />
+      </TouchableHighlight>
+      <TouchableHighlight style={styles.detailButton}>
+        <Image source={detailIcon} />
+      </TouchableHighlight>
+    </View>
+  </View>
+);
+
+ResultItem.propTypes = {
+  symbol: PropTypes.string.isRequired,
+  exchange: PropTypes.string.isRequired,
+};
 
 export default () => {
   const isLoading = useSelector((state) => state.search.isLoading);
@@ -19,23 +48,10 @@ export default () => {
     return <Text>{error.message}</Text>;
   }
   return (
-    <View>
-      {searchResult.map((info) => (
-        <View key={info.symbol} style={styles.resultList}>
-          <View>
-            <Text style={styles.stockName}>{info.symbol}</Text>
-            <Text style={styles.stockExchange}>{info.exchange}</Text>
-          </View>
-          <View style={styles.resultButton}>
-            <TouchableHighlight>
-              <Image source={tradeIcon} />
-            </TouchableHighlight>
-            <TouchableHighlight style={styles.detailButton}>
-              <Image source={detailIcon} />
-            </TouchableHighlight>
-          </View>
-        </View>
-      ))}
-    </View>
+    <FlatList
+      data={searchResult}
+      renderItem={({ item }) => <ResultItem symbol={item.symbol} exchange={item.exchange} />}
+      keyExtractor={(info) => info.symbol}
+    />
   );
 };
