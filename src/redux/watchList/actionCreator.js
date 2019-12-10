77adb9083c3fdf0fce650d2e74b267a811178fcd
xiaoxing <775132026@qq.com>
2019-12-10 16:39:53 +0800

    refactor arrow function

--- src/redux/watchList/actionCreator.js
+++ src/redux/watchList/actionCreator.js
@@ -1,19 +1,14 @@
 export const FETCH_WATCHLIST_SUCCESS = 'FETCH_WATCHLIST_SUCCESS';
 export const POST_WATCHLIST_SUCCESS = 'POST_WATCHLIST_SUCCESS';
 
-const fetchWatchList = () => ({
+export const fetchWatchList = () => ({
   type: FETCH_WATCHLIST_SUCCESS,
 });
 
-const postWatchList = (name) => ({
+export const postWatchList = (name) => ({
   type: POST_WATCHLIST_SUCCESS,
   payLoad: {
     name,
     stocks: [],
   },
 });
-
-export default {
-  fetchWatchList,
-  postWatchList,
-};
