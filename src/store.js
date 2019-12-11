3d160d52a27df4f90740a9098909fd081ad934f6
FairyWind <53415234+FairyWind@users.noreply.github.com>
2019-12-11 15:19:52 +0800

    Jsaal 12 (#19)
    * JSAAL-12 solve conflict and add whitelist
    
    * JSAAL-12 solve conflict and add whitelist
    
    * JSAAL-12 complete redux-persist

--- src/store.js
+++ src/store.js
@@ -1,14 +1,29 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
+
 import thunk from 'redux-thunk';
 import stockReducer from './redux/stock/reducer';
 import searchReducer from './redux/search/reducer';
 import watchlistsReducer from './redux/watchList/reducer';
 
-export default createStore(
-  combineReducers({
-    stock: stockReducer,
-    search: searchReducer,
-    watchlists: watchlistsReducer,
-  }),
+const reducer = combineReducers({
+  stock: stockReducer,
+  search: searchReducer,
+  watchlists: watchlistsReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['watchlists'],
+};
+
+const pReducer = persistReducer(persistConfig, reducer);
+
+export const store = createStore(
+  pReducer,
   applyMiddleware(thunk),
 );
+
+export const persistor = persistStore(store);
