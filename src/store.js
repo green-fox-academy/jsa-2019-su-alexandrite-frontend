21cfd4716e924aeb7f3a3a84567f662c8c938387
Qiongying Wu <qywumail@gmail.com>
2019-12-11 10:46:13 +0800

    JSAAL-12 solve conflict and add whitelist

--- src/store.js
+++ src/store.js
@@ -1,14 +1,26 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+
 import thunk from 'redux-thunk';
 import stockReducer from './redux/stock/reducer';
 import searchReducer from './redux/search/reducer';
 import watchlistsReducer from './redux/watchList/reducer';
 
-export default createStore(
-  combineReducers({
-    stock: stockReducer,
-    search: searchReducer,
-    watchlists: watchlistsReducer,
-  }),
+const reducer = combineReducers({
+  stock: stockReducer,
+  search: searchReducer,
+  watchList: watchlistsReducer,
+});
+
+const persistConfig = {
+  whitelist: ['watchList'],
+};
+
+const pReducer = persistReducer(persistConfig, reducer);
+
+export const store = createStore(
+  pReducer,
   applyMiddleware(thunk),
 );
+
+export const persistor = persistStore(store);
