1b4382b0c2c05f09f4de41996c40cb0e09695e9e
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-12-12 14:14:27 +0800

    added header style

--- src/stock-details/index.jsx
+++ src/stock-details/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {
+  Image,
   ScrollView,
+  TouchableHighlight,
 } from 'react-native';
 import { useNavigationParam } from 'react-navigation-hooks';
 import commonStyles from '../common/styles';
 import Stats from './Stats';
+import addToWatchlistIcon from '../../assets/icons/header/add-to-wl.png';
 
 const stockDetails = () => {
   const symbol = useNavigationParam('symbol');
@@ -18,6 +21,15 @@ const stockDetails = () => {
 
 stockDetails.navigationOptions = ({ navigation }) => ({
   title: navigation.getParam('symbol'),
+  headerStyle: {
+    backgroundColor: '#566ed3',
+  },
+  headerTintColor: '#fff',
+  headerRight: () => (
+    <TouchableHighlight>
+      <Image source={addToWatchlistIcon} />
+    </TouchableHighlight>
+  ),
 });
 
 export default stockDetails;
