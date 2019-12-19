3781ab21a9c8006615dfadaec412ce8a3bfd62c2
xiaoxing <775132026@qq.com>
2019-12-19 15:24:26 +0800

    JSAAL-15 remove signup file

--- src/Account/index.jsx
+++ src/Account/index.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { View, StatusBar, Text } from 'react-native';
-import Login from './Login';
+import Login from './SignIn';
 import styles from './styles';
 
 const navigationOptions = {
   title: 'Account',
 };
 
-const AccountScreen = () => {
-  return (
-    <View style={styles.container}>
-      <StatusBar
-        backgroundColor="blue"
-        barStyle="light-content"
-      />
-      <Text style={styles.title}>Welcome to login page</Text>
-      <Login />
-    </View>
-  );
-};
+const AccountScreen = () => (
+  <View style={styles.container}>
+    <StatusBar
+      backgroundColor="blue"
+      barStyle="light-content"
+    />
+    <Text style={styles.title}>Welcome to login page</Text>
+    <Login />
+  </View>
+);
 
 AccountScreen.navigationOptions = navigationOptions;
 
