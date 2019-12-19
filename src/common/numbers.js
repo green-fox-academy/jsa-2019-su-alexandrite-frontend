2a61101de74fea93cdad1562a8e59573c65c49cf
FairyWind <FairyWind@users.noreply.github.com>
2019-12-19 15:09:17 +0800

    JSAAL-17 fix eslint error in numbers.js

--- src/common/numbers.js
+++ src/common/numbers.js
@@ -15,12 +15,10 @@ export const processLargeNumbers = (n) => {
   return `${round(n / 1000000000)}B`;
 };
 
-export const addComma = (n) => {
-  return `${n}`
-    .split('.')[0]
-    .split('')
-    .reverse()
-    .map((c, i) => !i || i % 3 ? c : c + ',')
-    .reverse()
-    .join('');
-};
+export const addComma = (n) => `${n}`
+  .split('.')[0]
+  .split('')
+  .reverse()
+  .map((c, i) => (!i || i % 3 ? c : `${c},`))
+  .reverse()
+  .join('');
