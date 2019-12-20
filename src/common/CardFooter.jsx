8dfed9f75e1e82bcd7e1eb506350d1b187470a70
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-12-20 11:44:09 +0800

    added footer button

---
+++ src/common/CardFooter.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
+
+const CardFooter = ({ children }) => (
+  <View style={styles.cardFooter}>
+    {children}
+  </View>
+);
+
+
+CardFooter.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+};
+
+CardFooter.defaultProps = {
+  children: [],
+};
+
+export default CardFooter;
