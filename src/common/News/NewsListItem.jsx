fdcaa5a9a88eff62108404da86b4b3b4d4c209ec
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-12-20 14:42:23 +0800

    added webview for news

--- src/common/News/NewsListItem.jsx
+++ src/common/News/NewsListItem.jsx
@@ -6,20 +6,28 @@ import {
 } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import PropTypes from 'prop-types';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import { useNavigation } from 'react-navigation-hooks';
 import commonStyle from '../styles';
 
 const NewsListItem = ({ item, style }) => {
   const date = new Date(item.datetime);
+  const { push } = useNavigation();
   return (
-    <ImageBackground
-      source={item.image ? { uri: item.image } : null}
-      style={[commonStyle.newsCard, style]}
+    <TouchableHighlight
+      onPress={() => push('NewsWebView', { url: item.url })}
+      style={[style, { borderRadius: 6 }]}
     >
-      <LinearGradient style={commonStyle.newsCardOverlay} colors={['#0000', '#0000', '#0008', '#000f']}>
-        <Text style={commonStyle.newsTime}>{date.toLocaleString()}</Text>
-        <Text style={commonStyle.newsHeadline}>{item.headline}</Text>
-      </LinearGradient>
-    </ImageBackground>
+      <ImageBackground
+        source={item.image && { uri: item.image }}
+        style={commonStyle.newsCard}
+      >
+        <LinearGradient style={commonStyle.newsCardOverlay} colors={['#0000', '#0000', '#0008', '#000f']}>
+          <Text style={commonStyle.newsTime}>{date.toLocaleString()}</Text>
+          <Text style={commonStyle.newsHeadline}>{item.headline}</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableHighlight>
   );
 };
 
