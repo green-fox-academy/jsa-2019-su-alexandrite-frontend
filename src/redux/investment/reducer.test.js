609aae149d794e34d2bc083a1c62a81412d7d995
FairyWind <FairyWind@users.noreply.github.com>
2020-01-02 14:00:48 +0800

    test investment/reducer

---
+++ src/redux/investment/reducer.test.js
@@ -0,0 +1,50 @@
+import { FETCH_PORTFOLIO_DETAILS_START, FETCH_PORTFOLIO_DETAILS_FAIL, FETCH_PORTFOLIO_DETAILS_SUCCESS } from './actionType';
+import reducer from './reducer';
+
+test('return the initial state', () => {
+  expect(reducer(undefined, {})).toEqual({
+    isLoading: false,
+    totalValue: 0,
+    error: '',
+  });
+});
+
+test('FETCH_PORTFOLIO_DETAILS_START', () => {
+  expect(
+    reducer([], {
+      type: FETCH_PORTFOLIO_DETAILS_START,
+    }),
+  ).toEqual({
+    isLoading: true,
+    error: '',
+  });
+});
+
+test('handle FETCH_PORTFOLIO_DETAILS_SUCCESS', () => {
+  expect(
+    reducer([], {
+      type: FETCH_PORTFOLIO_DETAILS_SUCCESS,
+      payload: { totalValue: '80,000' },
+    }),
+  ).toEqual({
+    isLoading: false,
+    totalValue: '80,000',
+    error: '',
+  });
+});
+
+test('handle FETCH_PORTFOLIO_DETAILS_FAIL', () => {
+  expect(
+    reducer([], {
+      type: FETCH_PORTFOLIO_DETAILS_FAIL,
+      payload: {
+        message: ' The error Message',
+      },
+    }),
+  ).toEqual({
+    isLoading: false,
+    error: {
+      message: ' The error Message',
+    },
+  });
+});
