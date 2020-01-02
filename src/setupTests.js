1a1c2da2549bb33951f1658b997e13885eed8d15
Jiacheng Jiang <ajax15601560911@gmail.com>
2020-01-02 11:40:23 +0800

    fixed minor testing issue

--- src/setupTests.js
+++ src/setupTests.js
@@ -8,4 +8,22 @@ import '@testing-library/jest-dom/extend-expect';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+const mockStorage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+jest.mock('redux-persist/lib/storage', () => mockStorage);
+
+jest.mock('redux-persist', () => ({
+  persistReducer: jest.fn(() => { }),
+  persistStore: jest.fn(() => { }),
+}));
+
+jest.mock('redux', () => ({
+  combineReducers: jest.fn(() => { }),
+  createStore: jest.fn(() => { }),
+  applyMiddleware: jest.fn(() => { }),
+}));
+
 Enzyme.configure({ adapter: new Adapter() });
