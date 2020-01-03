7f0b58ad5281cd5c8a033db4e539d80dcb910a4c
FairyWind <FairyWind@users.noreply.github.com>
2020-01-03 15:03:52 +0800

    test add one test case for editButton

--- src/Watchlists/EditButton.test.jsx
+++ src/Watchlists/EditButton.test.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import EditButton from './EditButton';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<EditButton isInEditMode toggleEditMode={() => null} />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('<EditButton />', () => {
+  it('renders correctly when user watchlist is in edit mode', () => {
+    const tree = renderer.create(
+      <EditButton
+        isInEditMode
+        toggleEditMode={jest.fn()}
+      />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly when user watchlist is not in edit mode', () => {
+    const tree = renderer.create(
+      <EditButton
+        isInEditMode={false}
+        toggleEditMode={jest.fn()}
+      />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Should toggle edit mode when tapping on editorButton', () => {
+    const setIsInEditMode = jest.fn();
+    const toggleEditMode = jest.fn(() => setIsInEditMode);
+    const wrapper = shallow(
+      <EditButton
+        isInEditMode={false}
+        toggleEditMode={toggleEditMode}
+      />,
+    );
+    wrapper.find('TouchableHighlight')
+      .first()
+      .simulate('press');
+    expect(toggleEditMode).toHaveBeenCalledWith(true);
+  });
 });
