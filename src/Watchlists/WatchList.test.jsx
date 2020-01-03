a4fb6512cfb75c531442e3be6586ef398eab184a
xiaoxing <775132026@qq.com>
2020-01-03 17:23:11 +0800

    test finished watchlist component test

--- src/Watchlists/WatchList.test.jsx
+++ src/Watchlists/WatchList.test.jsx
@@ -2,39 +2,22 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
-import WatchListItem from './WatchListItem';
-import EditModeWatchListItem from './EditModeWatchListItem';
-import WatchList from './WatchList';
-import EditFooter from './EditFooter';
+import Watchlist from './WatchList';
 
 jest.mock('react-redux');
 jest.mock('../redux/watchList/actionCreator');
 jest.mock('../Watchlists/WatchListItem.jsx', () => '<WatchlistItem />');
 jest.mock('../Watchlists/EditModeWatchListItem.jsx', () => '<EditModeWatchListItem />');
-jest.mock('../Watchlists/WatchList.jsx', () => '<WatchList />');
 
-const mockedState = {
-  watchlists: [
-    {
-      id: 123,
-      name: 'Sample',
-      stocks: [
-        {
-          ticker: 'AAPL', currPrice: 123, dailyChange: 1.3, volume: '12B',
-        },
-        {
-          ticker: 'MSFT', currPrice: 123.1, dailyChange: -3.4, volume: '43M',
-        },
-      ],
-    },
-  ],
-  watchlistDetailsError: null,
-  isLoadingWatchlistDetails: false,
+const SAMPLE_STATE = {
+  isOpen: false,
+  isInEditMode: false,
+  checkedItems: [],
 };
 
-describe('<WatchList />', () => {
+describe('<Watchlist />', () => {
   beforeEach(() => {
-    useSelector.mockReturnValue(mockedState);
+    useSelector.mockReturnValue(SAMPLE_STATE);
     useDispatch.mockReturnValue(jest.fn(() => { }));
   });
 
@@ -45,46 +28,104 @@ describe('<WatchList />', () => {
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
 
-    const wrapper = shallow(<WatchList />);
+    const wrapper = shallow(<Watchlist item={{
+      id: 1,
+      name: 'app',
+      stocks: [
+        {
+          ticker: 'MSFT',
+          currPrice: 123.1,
+          dailyChange: -3.4,
+          volume: '43M',
+        },
+      ],
+    }}
+    />);
 
     expect(wrapper).not.toBeNull();
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('should render watchlistitem if the watchlist is not empty and match the snapshot', () => {
+  it('shoud render watchlist and match snapshot', () => {
     const tree = renderer.create(
-      <WatchListItem
-        ticker
-        currPrice
-        dailyChange
-        volume
+      <Watchlist item={{
+        id: 1,
+        name: 'app',
+        stocks: [],
+      }}
       />,
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('should render editmodewatchlistitem and match the snapshot', () => {
+  it('should render empty text and match snapshot when watchlist is empty', () => {
     const tree = renderer.create(
-      <EditModeWatchListItem
-        isChecked={false}
-        onSelect={jest.fn()}
-        ticker
+      <Watchlist item={{
+        id: 1,
+        name: 'app',
+        stocks: [],
+      }}
       />,
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('should render editfooter and match the snapshot', () => {
+  it('should render stock details and match snapshot when watchlist is not empty', () => {
     const tree = renderer.create(
-      <EditFooter
-        checkedItems={[]}
-        isInEditMode={false}
-        toggleEditMode={jest.fn()}
-        onDeleteWatchlist={jest.fn()}
-        onDeleteStocks={jest.fn()}
+      <Watchlist item={{
+        id: 1,
+        name: 'app',
+        stocks: [
+          {
+            ticker: 'MSFT',
+            currPrice: 123.1,
+            dailyChange: -3.4,
+            volume: '43M',
+          },
+        ],
+      }}
       />,
     ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
+  it('should render stock details and match snapshot when watchlist is open', () => {
+    useSelector.mockReturnValue({ ...SAMPLE_STATE, isOpen: false });
+    const tree = renderer.create(
+      <Watchlist item={{
+        id: 1,
+        name: 'app',
+        stocks: [
+          {
+            ticker: 'MSFT',
+            currPrice: 123.1,
+            dailyChange: -3.4,
+            volume: '43M',
+          },
+        ],
+      }}
+      />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render stock details and match snapshot when watchlist is not open', () => {
+    useSelector.mockReturnValue({ ...SAMPLE_STATE, isOpen: true });
+    const tree = renderer.create(
+      <Watchlist item={{
+        id: 1,
+        name: 'app',
+        stocks: [
+          {
+            ticker: 'MSFT',
+            currPrice: 123.1,
+            dailyChange: -3.4,
+            volume: '43M',
+          },
+        ],
+      }}
+      />,
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
