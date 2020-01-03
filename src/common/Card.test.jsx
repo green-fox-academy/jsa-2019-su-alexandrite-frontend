e3ff0884fe9ae9966284c5dafc4e695dc6243884
Jiacheng Jiang <ajax15601560911@gmail.com>
2020-01-03 15:15:39 +0800

    added title test for card

--- src/common/Card.test.jsx
+++ src/common/Card.test.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Text } from 'react-native';
 import renderer from 'react-test-renderer';
 import Card from './Card';
 
-test('Card rednder properly', () => {
+test('Card render properly', () => {
   const tree = renderer.create(
     <Card />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Card render properly with title', () => {
+  const tree = renderer.create(
+    <Card title="test title" />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Card render properly with children', () => {
+  const tree = renderer.create(
+    <Card><Text>test children</Text></Card>,
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
