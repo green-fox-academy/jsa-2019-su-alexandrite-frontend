9340a6a26921a423579490b657c38e995a7abf08
Jiacheng Jiang <ajax15601560911@gmail.com>
2020-01-07 10:27:00 +0800

    separated fetch news from news hook

--- src/common/News/useFetchStockNews.js
+++ src/common/News/useFetchStockNews.js
@@ -4,11 +4,7 @@ import {
   NEWS_API_KEY,
 } from 'react-native-dotenv';
 
-export default (query, page = 1, pageSize = 20) => {
-  const [news, setNews] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
+const fetchNews = async (query, page, pageSize) => {
   const newsUrl = new URL(`${NEWS_API_URL}/everything`);
   const parsedQuery = (typeof query.join) === 'function'
     ? query.join(' OR ')
@@ -19,33 +15,40 @@ export default (query, page = 1, pageSize = 20) => {
   newsUrl.searchParams.append('page', page);
   newsUrl.searchParams.append('sortBy', 'publishedAt');
   newsUrl.searchParams.append('language', 'en');
+  const res = await fetch(newsUrl);
+  if (!res.ok) {
+    switch (res.status) {
+      case 404:
+        throw new Error(`Cannot find any news about ${query}`);
+      case 426:
+        throw new Error(`No more news about ${query}, please come back later!`);
+      default:
+        throw new Error('Oops, there\'s something wrong with our app.');
+    }
+  }
+  const json = await res.json();
+  return json.articles.map(({
+    publishedAt, title, urlToImage, url,
+  }) => ({
+    datetime: publishedAt,
+    headline: title,
+    image: urlToImage,
+    url,
+    id: (new Date() * Math.random()).toString(),
+  }));
+};
+
+export default (query, page = 1, pageSize = 20) => {
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
     const fetchData = async () => {
       try {
-        const res = await fetch(newsUrl);
-        if (!res.ok) {
-          switch (res.status) {
-            case 404:
-              throw new Error(`Cannot find any news about ${query}`);
-            case 426:
-              throw new Error(`No more news about ${query}, please come back later!`);
-            default:
-              throw new Error('Oops, there\'s something wrong with our app.');
-          }
-        }
-        const json = await res.json();
-        const parsed = json.articles.map(({
-          publishedAt, title, urlToImage, url,
-        }) => ({
-          datetime: publishedAt,
-          headline: title,
-          image: urlToImage,
-          url,
-          id: (new Date() * Math.random()).toString(),
-        }));
-        setNews(page === 1 ? parsed : [...news, ...parsed]);
+        const result = await fetchNews(query, page, pageSize);
+        setNews(page === 1 ? result : [...news, ...result]);
         setIsLoading(false);
       } catch (err) {
         setError(err);
