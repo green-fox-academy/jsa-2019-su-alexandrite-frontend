26b56a0ce037b57a36c3b225272c4b3df7aaa621
FairyWind <FairyWind@users.noreply.github.com>
2020-01-07 15:58:46 +0800

    JSAAL-63 balance card

--- src/Account/index.jsx
+++ src/Account/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import commonStyles from '../common/styles';
+import Balance from './Balance';
 
 export default () => (
   <View style={commonStyles.container}>
     <Text>This is the account</Text>
+    <Balance />
   </View>
 );
