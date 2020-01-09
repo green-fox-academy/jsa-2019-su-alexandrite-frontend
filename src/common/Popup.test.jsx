846c644c3ac7b89cf8c13aaad3a196c468a1ebb4
Jiacheng Jiang <ajax15601560911@gmail.com>
2020-01-09 15:50:53 +0800

    added test for popup

---
+++ src/common/Popup.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+// import { shallow } from 'enzyme';
+// import { useNavigation } from 'react-navigation-hooks';
+
+import Popup, { PopupButtonGroup, PopupHeader } from './Popup';
+
+jest.mock('react-navigation-hooks');
+
+describe('<Popup />', () => {
+  it('should render properly and match the snapshot', () => {
+    const tree = renderer.create(
+      <Popup
+        visible={false}
+        onCancel={jest.fn()}
+        onConfirm={jest.fn()}
+        confirmButtonText="OK"
+        confirmDisabled={false}
+        title="Title"
+      />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render properly and match the snapshot when confirm enabled', () => {
+    const tree = renderer.create(
+      <Popup
+        visible={false}
+        onCancel={jest.fn()}
+        onConfirm={jest.fn()}
+        confirmButtonText="OK"
+        confirmDisabled
+        title="Title"
+      />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
