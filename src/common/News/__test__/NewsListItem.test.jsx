bba5b78736eabd90389258e4d7e376434599a68a
Jiacheng Jiang <ajax15601560911@gmail.com>
2020-01-09 17:15:52 +0800

    added test for news list item

---
+++ src/common/News/__test__/NewsListItem.test.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import { useNavigation } from 'react-navigation-hooks';
+import NewsListItem from '../NewsListItem';
+
+jest.mock('react-navigation-hooks');
+jest.mock('../useFetchStockNews');
+
+const sampleNewsItem = {
+  datetime: Date.now(),
+  headline: 'headline',
+  url: 'http://test.test',
+  image: 'http://test.test/test.jpg',
+};
+
+describe('<NewsListItem />', () => {
+  beforeEach(() => {
+    useNavigation.mockReturnValue({
+      push: jest.fn(),
+      navigate: jest.fn(),
+    });
+  });
+
+  afterAll(() => {
+    jest.resetModules();
+  });
+
+  it('Should render properly and match the snapshot', () => {
+    const tree = renderer.create(
+      <NewsListItem item={sampleNewsItem} />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+
+  it('Should render properly and match the snapshot when image url not present', () => {
+    const tree = renderer.create(
+      <NewsListItem item={{ ...sampleNewsItem, image: null }} />,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+
+  it('Should navigate to NewsWebView with url passed', () => {
+    const mockPush = jest.fn();
+    useNavigation.mockReturnValueOnce({
+      push: mockPush,
+    });
+    const wrapper = shallow(
+      <NewsListItem item={sampleNewsItem} />,
+    );
+    wrapper.find('TouchableHighlight').first().simulate('press');
+    expect(mockPush).toHaveBeenCalledWith('NewsWebView', { url: sampleNewsItem.url });
+  });
+});
