d4b38b5c615c514ed1053d573247287cf68c245d
FairyWind <FairyWind@users.noreply.github.com>
2020-01-10 10:11:11 +0800

    JSAAL-22 instrument styling

--- src/Investments/index.jsx
+++ src/Investments/index.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import commonStyles from '../common/styles';
 import SearchButton from '../common/HeaderSearchButton';
 import PortfolioValue from './PortfolioValue';
+import Instruments from './Instruments';
 
 const navigationOptions = {
   title: 'Investments',
   headerRight: () => <SearchButton />,
 };
 
-const Investments = () => (
-  <View style={commonStyles.container}>
-    <PortfolioValue />
-  </View>
-);
+const Investments = () => {
+  const { alignItems, ...rest } = commonStyles.container;
+  return (
+    <ScrollView style={rest} contentContainerStyle={{ alignItems }}>
+      <PortfolioValue />
+      <Instruments />
+    </ScrollView>
+  );
+};
 
 Investments.navigationOptions = navigationOptions;
 
