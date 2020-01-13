d62b26cd66d1e5dfc516f90b365894610af94319
FairyWind <FairyWind@users.noreply.github.com>
2020-01-13 12:03:33 +0800

    test add mock date

--- src/common/HeaderSearchButton.test.jsx
+++ src/common/HeaderSearchButton.test.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import { useNavigation } from 'react-navigation-hooks';
+import MockDate from 'mockdate';
 
 import SearchButton from './HeaderSearchButton';
 
 jest.mock('react-navigation-hooks');
+
+MockDate.set('2020-01-10T04:46:05.638Z');
 
 describe('<SearchButton />', () => {
   it('should call navigation.push when detail button is pressed', () => {
